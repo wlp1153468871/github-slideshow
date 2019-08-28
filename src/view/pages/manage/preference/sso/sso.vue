38bfe85b62a20d71c33e7f4e457eb77ccd6ba8d7
朱正浩,Zhu Zhenghao <zhenghao.zhu@daocloud.io>
2019-08-28 12:06:37 +0800

    refactor  sso module

--- src/view/pages/manage/preference/sso/sso.vue
+++ src/view/pages/manage/preference/sso/sso.vue
@@ -1,156 +1,194 @@
 <template>
-  <dao-setting-layout>
-    <div slot="layout-title">
-      上汽通用汽车统一身份管理
-    </div>
-
-    <dao-setting-section>
-      <div slot="label">名称</div>
-      <div slot="content">
-        <dao-input
-          icon-inside
-          name="name"
-          data-vv-as="名称"
-          v-validate="'required|max:20'"
-          :message="veeErrors.first('name')"
-          :status="veeErrors.has('name') ? 'error' : ''"
-          v-model="ssoModel.name">
-        </dao-input>
+  <div class="sso">
+    <dao-setting-layout>
+      <div slot="layout-title">
+        SSO 认证
       </div>
-    </dao-setting-section>
+      <dao-setting-section>
+        <template #content>
+          <el-table
+            v-loading="loading"
+            :data="rows">
+            <el-table-column
+              prop="name"
+              label="名称">
+            </el-table-column>
+            <el-table-column
+              show-overflow-tooltip
+              prop="login_url"
+              label="登录 URL">
+            </el-table-column>
+            <el-table-column
+              show-overflow-tooltip
+              prop="logout_url"
+              label="登出 URL">
+            </el-table-column>
+            <el-table-column
+              fixed="right"
+              label="操作"
+              align="center"
+              header-align="center"
+              width="80">
+              <template #default="{ row: record}">
+                <el-dropdown @command="handleOperate($event, record)">
+                  <span>
+                    <svg class="icon dropdown-trigger">
+                      <use xlink:href="#icon_more"></use>
+                    </svg>
+                  </span>
 
-    <dao-setting-section>
-      <template #label>登录 URL</template>
-      <template #content>
-        <dao-input
-          icon-inside
-          name="login_url"
-          data-vv-as="登录 URL"
-          v-validate="'required|url'"
-          :message="veeErrors.first('login_url')"
-          :status="veeErrors.has('login_url') ? 'error' : ''"
-          v-model="ssoModel.login_url">
-        </dao-input>
-      </template>
-    </dao-setting-section>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item
+                      command="edit"
+                      icon="el-icon-edit">
+                      编辑
+                    </el-dropdown-item>
+                    <el-dropdown-item
+                      class="dropdown-item-error"
+                      command="delete"
+                      icon="el-icon-delete">
+                      删除
+                    </el-dropdown-item>
+                  </el-dropdown-menu>
 
-    <dao-setting-section>
-      <template #label>登出 URL</template>
-      <template #content>
-        <dao-input
-          icon-inside
-          name="logout_url"
-          data-vv-as="登出 URL"
-          v-validate="'required|url'"
-          :message="veeErrors.first('logout_url')"
-          :status="veeErrors.has('logout_url') ? 'error' : ''"
-          v-model="ssoModel.logout_url">
-        </dao-input>
-      </template>
-    </dao-setting-section>
+                </el-dropdown>
+              </template>
+            </el-table-column>
+          </el-table>
+        </template>
+        <template #content-helper>
+          <button
+            class="dao-btn blue"
+            @click="openDialog">
+            添加
+          </button>
+        </template>
+      </dao-setting-section>
+      <dao-setting-section>
+        <div slot="label">只使用第三方登录</div>
+        <div slot="content">
+          <dao-switch
+            :option="{ on: '是', off: '否' }"
+            :with-notice="true"
+            v-model="enable_local_login"
+            @change="onChange">
+          </dao-switch>
+        </div>
+        <div slot="content-helper">您必须确保可以用第三方登录管理员账户, 否则任何人将无法登录</div>
+      </dao-setting-section>
+    </dao-setting-layout>
 
-    <dao-setting-section>
-      <template #label>仅通过 SSO 登录</template>
-      <template #content>
-        <dao-switch
-          @change="onChange"
-          name="sso-only"
-          v-model="ssoOnly">
-        </dao-switch>
-      </template>
-    </dao-setting-section>
-
-    <template #footer>
-      <save-button
-        class="blue"
-        text="保存"
-        :saving="isSubmit"
-        @click="onSubmit">
-      </save-button>
-    </template>
-
-  </dao-setting-layout>
+    <add-sso-dialog
+      :visible.sync="visible"
+      :base-model.sync="selectSSO"
+      @refresh="loadSSOInfo"
+    >
+    </add-sso-dialog>
+  </div>
 </template>
 
 <script>
-import { first, pick } from 'lodash';
 import SSOService from '@/core/services/sso.service';
+import addSsoDialog from '@/view/pages/dialogs/preference/add-sso';
 
 export default {
-  name: 'SSODetail',
+  name: 'SSOList',
 
-  data() {
-    return {
-      ssoModel: {},
-      isSubmit: false,
-      ssoOnly: false,
-    };
+  components: {
+    addSsoDialog,
   },
 
   created() {
     this.loadSSOInfo();
   },
 
+  data() {
+    return {
+      rows: [],
+      selectSSO: null,
+      visible: false,
+      enable_local_login: false,
+      loading: true,
+    };
+  },
+
   methods: {
     loadSSOInfo() {
+      this.loading = true;
       SSOService.getSSO().then(sso => {
-        this.ssoOnly = !sso.enable_local_login;
+        this.enable_local_login = !sso;
       });
+      SSOService.getIdentityProvider()
+        .then(providers => {
+          this.rows = providers;
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
 
-      SSOService.getIdentityProvider().then(providers => {
-        this.ssoModel = pick(first(providers), [
-          'id',
-          'name',
-          'login_url',
-          'logout_url',
-        ]);
+    openDialog() {
+      this.visible = true;
+      console.log(this.visible);
+    },
+
+    handleOperate(command, sso) {
+      if (command === 'edit') {
+        this.selectSSO = sso;
+        this.openDialog();
+      }
+      if (command === 'delete') {
+        this.confirmDeleteSSO(sso);
+      }
+    },
+
+    confirmDeleteSSO(sso) {
+      this.$tada
+        .confirm({
+          title: '删除第三方登录',
+          text: `您确定要删除第三方登录 ${sso.name} 吗？`,
+        })
+        .then(willDel => {
+          if (willDel) {
+            this.deleteSSO(sso.id);
+          }
+        });
+    },
+
+    deleteSSO(ssoId) {
+      SSOService.deleteIdentityProvider(ssoId).then(() => {
+        this.$noty.success('删除第三方登录成功!');
+        this.loadSSOInfo();
       });
     },
 
     onChange() {
-      SSOService.updateSSO({
-        enable_local_login: !this.ssoOnly,
-      });
-    },
-
-    onSubmit() {
-      this.$validator.validateAll().then(valid => {
-        if (valid) {
-          this.isSubmit = true;
-          if (this.ssoModel.id) {
-            this.updateSSO();
-          } else {
-            this.createSSO();
-          }
-        }
-      });
-    },
-
-    createSSO() {
-      SSOService.createIdentityProvider(this.ssoModel)
-        .then(() => {
-          this.$noty.success('添加成功');
-        })
-        .catch(() => {
-          this.$noty.error('添加第三方登录失败');
-        })
-        .finally(() => {
-          this.isSubmit = false;
-        });
-    },
-
-    updateSSO() {
-      SSOService.updateIdentityProvider(this.ssoModel.id, this.ssoModel)
-        .then(() => {
-          this.$noty.success('修改成功');
-        })
-        .catch(() => {
-          this.$noty.error('修改第三方登录失败');
-        })
-        .finally(() => {
-          this.isSubmit = false;
-        });
+      if (!this.rows.length && !this.loadings.sso) {
+        this.$tada.error('请先至少添加一条 SSO 登录信息!');
+        this.enable_local_login = false;
+      } else {
+        const params = {
+          enable_local_login: !this.enable_local_login,
+        };
+        SSOService.updateSSO(params);
+      }
     },
   },
 };
 </script>
+
+<style lang="scss">
+.sso {
+  .access-keys-table {
+    overflow-x: auto;
+
+    .dao-table {
+      table-layout: fixed;
+    }
+  }
+
+  a.icon {
+    cursor: pointer;
+  }
+}
+</style>
