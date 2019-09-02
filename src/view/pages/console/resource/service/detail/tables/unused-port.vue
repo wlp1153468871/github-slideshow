8ef8e377486d0e27f89a6d1220429952eee785d7
朱正浩,Zhu Zhenghao <zhenghao.zhu@daocloud.io>
2019-09-02 15:03:44 +0800

    refactor service to look good in env-k8s

---
+++ src/view/pages/console/resource/service/detail/tables/unused-port.vue
@@ -0,0 +1,45 @@
+<template>
+  <tr>
+    <td>
+      <span v-if="!port.nodePort" class="text-muted">暂无</span>
+      <span v-if="port.nodePort">{{port.nodePort}}</span>
+    </td>
+
+    <td role="presentation" class="text-muted text-center">
+      <svg class="icon text-muted">
+        <use xlink:href="#icon_arrow-right"></use>
+      </svg>
+    </td>
+
+    <td data-title="Service Port">
+      {{port.port}}/{{port.protocol}}
+      <span v-if="port.name">({{port.name}})</span>
+    </td>
+
+    <td role="presentation" class="text-muted text-center">
+      <svg class="icon text-muted">
+        <use xlink:href="#icon_arrow-right"></use>
+      </svg>
+    </td>
+
+    <td data-title="Target Port">
+      {{port.targetPort}}
+    </td>
+
+    <td data-title="Hostname"><span class="text-muted">暂无</span></td>
+
+    <td data-title="Termination">
+      <span class="text-muted">暂无</span>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'UnusedPort',
+
+  props: {
+    port: { type: Object },
+  },
+};
+</script>
