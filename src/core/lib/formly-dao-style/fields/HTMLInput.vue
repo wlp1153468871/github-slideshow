b4b5dd4433af7ca6aea05cbd59282203aaf0d168
Joan Wu <qiong.wu@daocloud.io>
2019-09-24 18:48:48 +0800

    update lib folder

---
+++ src/core/lib/formly-dao-style/fields/HTMLInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <field-item :to="to">
+    <input
+      :type="inputType"
+      class="dao-control"
+      v-model="model[field.key]"
+      :placeholder="getAtts(to.atts, 'placeholder')"
+      :disabled="to.disabled"
+    />
+  </field-item>
+</template>
+
+<script>
+import baseField from './baseField';
+import FieldItem from '../components/field-item';
+
+export default {
+  components: {
+    FieldItem,
+  },
+
+  mixins: [baseField],
+
+
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
+  },
+
+  computed: {
+    inputType() {
+      const { attrs = ({}) } = this.field;
+      if (attrs.inputType) {
+        return attrs.inputType;
+      }
+      return this.type;
+    },
+  },
+};
+</script>
---
+++ src/core/lib/formly-dao-style/fields/switch.vue
@@ -0,0 +1,24 @@
+<template>
+  <field-item :to="to">
+    <dao-switch
+      :class="to.classes"
+      :id="to.id ? to.id : null"
+      :option="to.option"
+      :with-notice="true"
+      v-model="model[field.key]">
+    </dao-switch>
+  </field-item>
+</template>
+
+<script>
+import baseField from './baseField';
+import FieldItem from '../components/field-item';
+
+export default {
+  components: {
+    FieldItem,
+  },
+
+  mixins: [baseField],
+};
+</script>
