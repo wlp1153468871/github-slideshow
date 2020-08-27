5359e1dcfe7dd74e4a66d6dd75fa346dd9594eea
1970186643 <1970186643@qq.com>
2020-08-27 16:01:54 +0800

    应用管理接口对接

--- src/core/services/service-admin.service.js
+++ src/core/services/service-admin.service.js
@@ -1,3 +1,4 @@
+import AuthService from '@/core/services/auth.service';
 import api from './api';
 
 class ServiceAdmin {
@@ -28,11 +29,84 @@ class ServiceAdmin {
   }
 
   /**
-   * 根据app_id获取一个应用
+   * 获取charts（版本）列表
    * @param {String} app_id
    */
   async getCharts(app_id) {
     return this.api.get(`/appstore/applications/${app_id}/application_infos`);
+  }
+
+  /**
+   * 获取app下项目组列表
+   * @param {String} app_id
+   */
+  async getOrganizations(app_id) {
+    return this.api.get(`/appstore/applications/${app_id}/spaces`);
+  }
+
+  /**
+   * 获取应用下的实例列表
+   * @param {String} app_id
+   */
+  async getInstances(app_id) {
+    return this.api.get(`/appstore/applications/${app_id}/instances`);
+  }
+
+  /**
+   * 获取应用分类列表
+   */
+  async getCategory() {
+    return this.api.get('/appstore/applications/categories');
+  }
+
+  /**
+   * 更新应用信息
+   * @param {String} app_id
+   */
+  async updateApp(app_id, data) {
+    return this.api.patch(`/appstore/applications/${app_id}`, data);
+  }
+  /**
+   * 上传图片
+   * @param {String} data 图片数据
+   */
+  get token() {
+    return AuthService.getToken();
+  }
+  uploadPic(file) {
+    return this.api
+      .request({
+        url: '/blobs',
+        method: 'post',
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+          'Content-Type': file.type,
+        },
+        data: file,
+      })
+      .then(res => {
+        return res;
+      })
+      .catch(() => {
+        console.log('上传出错了');
+      });
+  }
+
+  /**
+   * 上传chart新版本
+   * @param {String} zone_id
+   * @param {Array} data
+   */
+  async uploadFile(zone_id, app_id, data) {
+    return this.api.post(`/appstore/zones/${zone_id}/applications/${app_id}/application_infos`, data);
+  }
+  /**
+   * 上传chart新版本
+   * @param {String} app_id
+   * @param {Array} data
+   */
+  async unavailableApp(app_id) {
+    return this.api.patch(`/appstore/applications/${app_id}/unavailable`);
   }
 }
 
--- src/view/pages/manage/application/detail/detail.vue
+++ src/view/pages/manage/application/detail/detail.vue
@@ -47,7 +47,16 @@
         </button>
         <dao-dropdown-menu slot="list" style="min-width: 100px;">
           <dao-dropdown-item style="margin-left: 10px">
+            <span @click="editInfo">编辑基本信息</span>
+          </dao-dropdown-item>
+          <dao-dropdown-item style="margin-left: 10px">
+            <span @click="addEdition">添加版本</span>
+          </dao-dropdown-item>
+          <dao-dropdown-item style="margin-left: 10px" v-if="appInfo.available">
             <span>下架应用</span>
+          </dao-dropdown-item>
+          <dao-dropdown-item style="margin-left: 10px" v-else>
+            <span>上架应用</span>
           </dao-dropdown-item>
           <dao-dropdown-item style="margin-left: 10px">
             <span style="color: red;">删除</span>
@@ -55,6 +64,140 @@
         </dao-dropdown-menu>
       </dao-dropdown>
     </div>
+    <dao-dialog
+      :visible.sync="configEdit"
+      header="编辑基本信息"
+    >
+      <div class="dao-setting-layout">
+        <div class="dao-setting-section" style="padding: 20px;">
+          <div class="dao-setting-item">
+            <div class="dao-setting-label dao-name">自定义应用名称</div>
+            <div class="dao-setting-content">
+              <dao-input
+                v-model="form.name"
+                block
+                :disabled="appInfo.isGlobal"
+                style="width: 98%"
+                placeholder="请输入应用名称">
+              </dao-input>
+            </div>
+          </div>
+        </div>
+        <div class="dao-setting-section">
+          <div class="dao-setting-item">
+            <div class="dao-setting-label dao-name">应用图标</div>
+            <div class="dao-setting-content">
+              <div class="desc">建议大小120 像素 x 120 像素，支持 PNG，文件小于 1 MB</div>
+              <div v-show="isShow">
+                <img
+                  :src="`http://jizhidev.k8s01.ats${appInfo.pictureUrl}`"
+                  alt="应用图标"
+                  class="pic"
+                  v-if="appInfo.pictureId"/>
+                <img
+                  src="@/assets/images/card-Default.png"
+                  class="pic"
+                  v-else/>
+                <div>
+                  <button
+                    class="dao-btn red"
+                    @click="changeShow">
+                    删除
+                  </button>
+                </div>
+              </div>
+              <el-upload
+                v-show="!isShow"
+                class="upload-demo"
+                ref="upload"
+                action="#"
+                :http-request="handleUpload"
+                :file-list="fileList"
+                accept="image/png"
+                :limit="1"
+                :before-upload="beforeUpload"
+                :on-remove="removeFile">
+                <button class="dao-btn blue">上传图标</button>
+              </el-upload>
+            </div>
+          </div>
+        </div>
+        <div class="dao-setting-section">
+          <div class="dao-setting-item">
+            <div class="dao-setting-label dao-name">分类</div>
+            <div class="dao-setting-content">
+              <el-select
+                v-model="form.category"
+                multiple
+                default-first-option
+                placeholder="选择分类"
+                style="width: 98%;">
+                <el-option
+                  v-for="item in categories"
+                  :key="item.name"
+                  :label="item.name"
+                  :value="item.id">
+                </el-option>
+              </el-select>
+            </div>
+          </div>
+        </div>
+        <div class="dao-setting-section">
+          <div class="dao-setting-item">
+            <div class="dao-setting-label dao-name">描述</div>
+            <div class="dao-setting-content">
+              <textarea
+                class="dao-control"
+                type="text"
+                placeholder="添加描述"
+                rows="2"
+                v-model="form.description"
+                style="width: 98%; height:20px"
+              >
+              </textarea>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div slot="footer">
+        <button class="dao-btn ghost" @click="editClose">
+          取消
+        </button>
+        <button class="dao-btn blue" @click="handleUpload">
+          确认
+        </button>
+      </div>
+    </dao-dialog>
+    <dao-dialog
+      :visible.sync="configAdd"
+      header="添加版本"
+    >
+      <div class="dao-setting-layout">
+        <div class="dao-setting-section" style="padding: 20px;">
+          <div class="dao-setting-item">
+            <div class="dao-setting-label dao-name">Chart 文件</div>
+            <div class="dao-setting-content">
+              <el-upload
+                class="upload-demo"
+                ref="upload"
+                action="#"
+                :http-request="handleUploadChart"
+                :file-list="chartList"
+                accept="application/zip, application/x-compressed, application/x-gzip"
+                :limit="1"
+                :before-upload="beforeUploadChart"
+                :on-remove="removeFile">
+                <button class="dao-btn blue">上传chart</button>
+              </el-upload>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div slot="footer">
+        <button class="dao-btn ghost" @click="addClose">取消</button>
+        <button class="dao-btn blue" @click="handleUploadChart">确认</button>
+      </div>
+    </dao-dialog>
     <el-tabs v-model="activeName" style="position:relative;">
       <el-tab-pane label="概览" name="first">
         <div class="lay-left">
@@ -155,18 +298,29 @@
         <div class="project">
           状态：
           <dao-select
-            v-model="item"
+            v-model="select"
             size="sm"
             class="select"
           >
-            <!-- <dao-option
-              v-for="item in items"
-              :key="item.value"
-              :value="item.text"
-              :label="item.text">
-            </dao-option> -->
+            <dao-option
+              v-for="item in status"
+              :key="item.available"
+              :value="item.available"
+              :label="item.value">
+            </dao-option>
           </dao-select>
-          <span class="icon">
+          <span class="selectStatus" v-if="selectStatus.length">
+            <span class="number">已选择 1 项</span>
+            <button
+              class="dao-btn status"
+              v-if="selectStatus[0] === '0' && selectStatus.length === 1">批量上架
+            </button>
+            <button
+              class="dao-btn status"
+              v-if="selectStatus[0] === '1' && selectStatus.length === 1">批量下架
+            </button>
+          </span>
+          <span class="iconlay">
             <button class="dao-btn icon-btn" style="margin-left: 10px;">
               <svg class="icon"><use xlink:href="#icon_cw"></use></svg>
             </button>
@@ -180,13 +334,63 @@
           <div style="margin-top: 20px;">
             <el-table
               style="width: 100%;"
-              :data="tableData"
+              :data="orginization"
+              @select="selectChange"
+              @selection-change="selectAll"
             >
-              <el-table-column label="项目组名" prop="groupName"></el-table-column>
-              <el-table-column label="项目组唯一标识符" prop="groupUnique"></el-table-column>
-              <el-table-column label="租户名" prop="tenantName"></el-table-column>
-              <el-table-column label="租户唯一标识符" prop="tenantUnique"></el-table-column>
-              <el-table-column label="添加日期" prop="date"></el-table-column>
+              <el-table-column type="selection" width="50"></el-table-column>
+              <el-table-column label="项目组名" prop="name"></el-table-column>
+              <el-table-column label="状态">
+                <template slot-scope="scope">
+                  <div v-if="scope.row.available === '1'">
+                    <svg class="icon" style="color: #25D473">
+                      <use :xlink:href="`#icon_status-dot-small`"></use>
+                    </svg>
+                    <span>已上架</span>
+                  </div>
+                  <div v-else>
+                    <svg class="icon" style="color: rgb(204, 209, 217)">
+                      <use :xlink:href="`#icon_status-dot-small`"></use>
+                    </svg>
+                    <span>已下架</span>
+                  </div>
+                </template>
+              </el-table-column>
+              <el-table-column label="项目组唯一标识符" prop="shortName">
+                <template slot-scope="scope">
+                  <div style="color: #217EF2;cursor: pointer;">
+                    {{ scope.row.shortName }}
+                  </div>
+                </template>
+              </el-table-column>
+              <el-table-column label="租户名" prop="organizationName"></el-table-column>
+              <!-- <el-table-column label="租户唯一标识符" prop="tenantUnique"></el-table-column> -->
+              <el-table-column label="操作" width="80">
+                <template slot-scope="scope">
+                  <span class="dao-btn-group select-btn">
+                    <dao-dropdown
+                      trigger="click"
+                      :append-to-body="true"
+                      placement="bottom-start"
+                    >
+                      <svg class="icon">
+                        <use :xlink:href="`#icon_more`"></use>
+                      </svg>
+                      <dao-dropdown-menu slot="list" style="min-width: 120px;">
+                        <dao-dropdown-item
+                          style="margin-left: 10px"
+                          v-if="scope.row.available === '1'"
+                        >
+                          <span>下架应用</span>
+                        </dao-dropdown-item>
+                        <dao-dropdown-item style="margin-left: 10px" v-else>
+                          <span>下架应用</span>
+                        </dao-dropdown-item>
+                      </dao-dropdown-menu>
+                    </dao-dropdown>
+                  </span>
+                </template>
+              </el-table-column>
             </el-table>
             <div class="footer">
               <div class="page">共 1项</div>
@@ -231,21 +435,37 @@
         <div style="margin-top: 20px;">
           <el-table
             style="width: 100%;"
-            :data="tableData1"
+            :data="instances"
           >
-            <el-table-column label="实例名" prop="exampleName"></el-table-column>
-            <el-table-column label="Chart 版本" prop="chartType" width="100"></el-table-column>
+            <el-table-column label="实例名" prop="name"></el-table-column>
+            <el-table-column label="Chart 版本" prop="chartVersion" width="100"></el-table-column>
             <el-table-column label="租户/项目组" prop="tenant"></el-table-column>
             <el-table-column label="状态" width="100">
               <template slot-scope="scope">
                 <svg class="icon" style="color: #25D473">
                   <use :xlink:href="`#icon_status-dot-small`"></use>
                 </svg>
-                <span>{{ scope.row.state }}</span>
+                <span>{{ scope.row.status }}</span>
+                <!-- <div v-if="scope.row.available === '1'">
+                  <svg class="icon" style="color: #25D473">
+                    <use :xlink:href="`#icon_status-dot-small`"></use>
+                  </svg>
+                  <span>已上架</span>
+                </div>
+                <div v-else>
+                  <svg class="icon" style="color: rgb(204, 209, 217)">
+                    <use :xlink:href="`#icon_status-dot-small`"></use>
+                  </svg>
+                  <span>已下架</span>
+                </div> -->
               </template>
             </el-table-column>
-            <el-table-column label="创建者" prop="creator"></el-table-column>
-            <el-table-column label="创建日期" prop="date"></el-table-column>
+            <el-table-column label="创建者" prop="ownerName"></el-table-column>
+            <el-table-column label="创建日期">
+              <template slot-scope="scope">
+                {{ scope.row.createdAt | unix_date('YYYY/MM/DD HH:mm:ss') }}
+              </template>
+            </el-table-column>
           </el-table>
           <div class="footer">
             <div class="page">共 1 项</div>
