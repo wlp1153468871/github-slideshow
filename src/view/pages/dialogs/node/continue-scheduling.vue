9e5eaf93c9a10b0b6edf556250bb84e265a0aaa8
ly283913519 <283913519@qq.com>
2020-09-01 11:49:56 +0800

    fix:【DSP-DCE】【节点管理】控制继续调度和暂停调度按钮不能重复点击

--- src/view/pages/dialogs/node/continue-scheduling.vue
+++ src/view/pages/dialogs/node/continue-scheduling.vue
@@ -6,7 +6,8 @@
     <div class="pt-lg pb-lg pl-lg pr-lg">此操作将节点标记为可调度的。新应用将可以被调度到该节点。</div>
     <div slot="footer">
       <button class="dao-btn ghost" @click="onClose">取消</button>
-      <button class="dao-btn red" @click="onConfirm">确定</button>
+       <save-button class="dao-btn red" text="确定" :saving="isSaving" @click="onConfirm">
+      </save-button>
     </div>
   </dao-dialog>
 </template>
@@ -20,18 +21,26 @@ export default {
   name: 'ContinueSchedulingDialog',
   extends: Dialog,
   props: ['visible', 'node', 'zoneId'],
+  data() {
+    return {
+      isSaving: false,
+    };
+  },
   methods: {
     onConfirm() {
+      this.isSaving = true;
       this.changedData = cloneDeep(this.node);
       this.changedData.spec.unschedulable = false;
       NodeService
         .updateNode(this.node.metadata.name, this.changedData, this.zoneId)
         .then(() => {
+          this.isSaving = false;
           this.$noty.success(`主机 ${this.node.metadata.name} 成功继续调度`);
           this.onClose();
           this.$emit('updatetNodeList');
         })
         .catch(() => {
+          this.isSaving = false;
           this.$noty.error(`主机 ${this.node.metadata.name} 继续调度失败`);
         });
     },
--- src/view/pages/dialogs/node/scheduling-disabled.vue
+++ src/view/pages/dialogs/node/scheduling-disabled.vue
@@ -6,7 +6,8 @@
     <div class="p-lg">此操作将节点标记为不可调度的。新应用将不会被调度到该节点。</div>
     <div slot="footer">
       <button class="dao-btn ghost" @click="onClose">取消</button>
-      <button class="dao-btn red" @click="onConfirm">确定</button>
+      <save-button class="dao-btn red" text="确定" :saving="isSaving" @click="onConfirm">
+      </save-button>
     </div>
   </dao-dialog>
 </template>
@@ -23,20 +24,24 @@ export default {
   data() {
     return {
       changedData: [],
+      isSaving: false,
     };
   },
   methods: {
     onConfirm() {
+      this.isSaving = true;
       this.changedData = cloneDeep(this.node);
       this.changedData.spec.unschedulable = true;
       NodeService
         .updateNode(this.node.metadata.name, this.changedData, this.zoneId)
         .then(() => {
+          this.isSaving = false;
           this.$noty.success(`主机 ${this.node.metadata.name} 成功暂停调度`);
           this.onClose();
           this.$emit('updatetNodeList');
         })
         .catch(() => {
+          this.isSaving = false;
           this.$noty.error(`主机 ${this.node.metadata.name} 暂停调度失败`);
         });
     },
